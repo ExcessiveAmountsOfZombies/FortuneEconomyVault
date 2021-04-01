b3b8a641c41f2f33e40ce98da92d5b6c8b1c33a9
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2021-03-31 20:55:19 -0500

    add pages to baltop command

--- src/main/java/com/epherical/fortune/impl/command/BaltopCommand.java
+++ src/main/java/com/epherical/fortune/impl/command/BaltopCommand.java
@@ -9,7 +9,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
 import net.milkbowl.vault.economy.Economy;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.CommandSender;
 
 import java.util.Comparator;
@@ -40,31 +39,38 @@ public class BaltopCommand extends BaseCommand {
 
     @CommandAlias("baltop|moneytop|balancetop")
     @Description("See who has the most money on the server.")
+    @CommandCompletion("1")
     //@CommandPermission("fconomy.command.baltop")
-    private void baltop(CommandSender source) {
+    private void baltop(CommandSender source, String[] args) {
         try {
+            int page = 1;
+            if (args.length >= 1) {
+                try {
+                    page = Integer.parseInt(args[0]);
+                } catch (NumberFormatException ignored) {}
+            }
+
             List<EconomyUser> users = data.users();
             users.sort(Comparator.comparingDouble(EconomyUser::currentBalance).reversed());
-
-            int page = 1;
-
-            int counter = 1;
+            page = Math.max(page, 1);
+            int maxPage = Math.max(users.size() / 10, 1);
+            maxPage = users.size() % 10 != 0 ? maxPage + 1 : maxPage;
+            int counter = page == 1 ? 1 : ((page -1) * 10) + 1;
 
             // -=- Page 1/1 -=- Top Balances -=-=-
             Component msg = Component.text("-=- ").style(Style.style(TextColor.fromHexString("#8f8f8f")))
                     .append(Component.text("Page ").style(Style.style(TextColor.fromHexString("#8f8f8f"))))
                     .append(Component.text(page).style(Style.style(TextColor.fromHexString("#545454"))))
                     .append(Component.text("/").style(Style.style(TextColor.fromHexString("#8f8f8f"))))
-                    .append(Component.text(page).style(Style.style(TextColor.fromHexString("#545454"))))
+                    .append(Component.text(maxPage).style(Style.style(TextColor.fromHexString("#545454"))))
                     .append(Component.text(" -=- Top Balances -=-=-").style(Style.style(TextColor.fromHexString("#8f8f8f"))));
 
             source.sendMessage(msg, MessageType.SYSTEM);
+            int begin = page == 1 ? 0 : Math.min(users.size(), ((page -1) * 10));
+            int end = page == 1 ? 10 : Math.min(users.size(), (page * 10));
 
-
-            for (EconomyUser user : users) {
+            for (EconomyUser user : users.subList(begin, end)) {
                 String money = economy.format(user.currentBalance());
-
-
 
                 Component row = Component.text(counter + ". ")
                         .append(Component.text(user.name() + " ").style(Style.style())
